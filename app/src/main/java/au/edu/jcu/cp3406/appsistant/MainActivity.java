4ecd6c317e00bef682dcf157e6f6b416afb6ec62
Christgo <christopher.ordorica@my.jcu.edu.au>
2020-08-27 15:01:40 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/appsistant/MainActivity.java
@@ -0,0 +1,14 @@
+package au.edu.jcu.cp3406.appsistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
