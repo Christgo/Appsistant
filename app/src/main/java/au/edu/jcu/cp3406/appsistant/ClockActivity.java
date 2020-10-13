21c8903ec00f2754f659079e1c058811c390357b
Chelsea Lynch <chelsea171100@gmail.com>
2020-10-13 15:21:50 +1000

    Add code for timer button

--- app/src/main/java/au/edu/jcu/cp3406/appsistant/ClockActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/appsistant/ClockActivity.java
@@ -1,11 +1,23 @@
 package au.edu.jcu.cp3406.appsistant;
 
 import android.content.Intent;
+import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
 
-public class ClockActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    public void onClick() {
-        Intent worldClockIntent = new Intent();
+public class ClockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+
+    public void onTimerButtonClicked(View view) {
+        Intent timerIntent = new Intent(this, TimerActivity.class);
+        startActivity(timerIntent);
     }
 
 }
