979a9e51e02619709f67b9be10ae99b893a02599
jerry <jie.peng@my.jcu.edu.au>
2020-10-30 23:29:13 +1000

    add xml for AddContacts file

---
+++ app/src/main/java/au/edu/jcu/cp3406/appsistant/AddContacts.java
@@ -0,0 +1,12 @@
+package au.edu.jcu.cp3406.appsistant;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddContacts extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcontacts);}
+}
